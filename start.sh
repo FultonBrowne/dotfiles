f9d046b81fdbc46a8ba45f0806dfd85fb6ea3d74
I use arch btw <gprlabs@outlook.com>
2020-07-26 22:31:45 +0000

    added install script

---
+++ start.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+sudo pacman -S binutils make gcc fakeroot
+echo "Building Fulton's config"
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -S - < pkglist.txt --needed
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+sudo systemctl enable ly.service
+cp .zshrc ~/.zshrc
+cd $HOME
+git clone https://github.com/adi1090x/dynamic-wallpaper.git
+cd dynamic-wallpaper
+chmod +x install.sh
+./install.sh
